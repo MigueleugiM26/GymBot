5dbbb25d34c6eb7117bb9a465fc42678a3f3816c
Miguel Gabriel <srtjmg@gmail.com>
2025-01-27 22:09:34 -0300

    Monster Skills

--- commands/dungeon.py
+++ commands/dungeon.py
@@ -9,7 +9,6 @@ from commands.globalFunctions import load_user_data, save_user_data
 with open("storage/enemies.json", "r") as file:
     enemies_data = json.load(file)
 
-
 def generate_health_bar(current_hp, bar_length=10):
     filled_length = round(bar_length * current_hp / max_hp) 
     empty_length = bar_length - filled_length
@@ -51,11 +50,12 @@ class ReviveView(View):
             await interaction.response.defer()
 
 class EnemyView(View):
-    def __init__(self, user_entry, enemy_name, enemy_stats, interaction):
+    def __init__(self, user_entry, enemy_name, enemy_stats, statuses, interaction):
         super().__init__()
         self.user_entry = user_entry
         self.enemy_name = enemy_name
         self.enemy_stats = enemy_stats
+        self.statuses = statuses
         self.interaction = interaction
 
     @discord.ui.button(label="Continue", style=discord.ButtonStyle.danger)
@@ -85,104 +85,207 @@ class EnemyView(View):
             inline=False
             )
 
-            view = PlayerView(self.user_entry, self.enemy_name, self.enemy_stats, self.interaction)
+            view = PlayerView(self.user_entry, self.enemy_name, self.enemy_stats, self.statuses, self.interaction)
             await interaction.message.edit(embed=embed, view=view)
         else:
-            baseDamage = self.enemy_stats["attack"]
-            critical = False
+            if self.statuses["hasStatus"] and random.randint(0, 100) <= 5:
+                baseDamage = self.enemy_stats["attack"]
+                critical = False
 
-            if random.randint(0, 100) <= 5:
-                baseDamage = baseDamage * 2
-                critical = True
+                if random.randint(0, 100) <= 85:
+                    baseDamage = baseDamage * 2
+                    critical = True
 
-            self.user_entry["hp"] -= baseDamage
-            self.user_entry["hp"] = max(0, self.user_entry["hp"])
+                self.user_entry["hp"] -= baseDamage
+                self.user_entry["hp"] = max(0, self.user_entry["hp"])
 
-            if self.user_entry["hp"] <= 0:
-                reviveChance = max(20, self.user_entry["endurance"] / 10)
+                if self.user_entry["hp"] <= 0:
+                    reviveChance = max(20, self.user_entry["endurance"] / 10)
 
-                randomRoll = random.randint(0, 100)
+                    randomRoll = random.randint(0, 100)
 
-                if randomRoll <= reviveChance:
-                    self.user_entry["hp"] += 1
+                    if randomRoll <= reviveChance:
+                        self.user_entry["hp"] += 1
 
-                    embed = discord.Embed (
-                        title=f"The **{self.enemy_name}** has attacked you!"
+                        embed = discord.Embed (
+                            title=f"The **{self.enemy_name}** has attacked you!"
+                        )
+                        embed.set_thumbnail(url=self.enemy_stats["image"])
+                        if critical:
+                            embed.add_field(name="", value=f"**Critical hit!** You suffer {baseDamage} damage!", inline=False)
+                        else:
+                            embed.add_field(name="", value=f"You suffer {baseDamage} damage!", inline=False)
+                        embed.add_field(name="", value=f"The **{self.enemy_name}** has knocked you down, but your muscles won't give up! You recover 1 HP!", inline=False)
+                        view = ReviveView(self.user_entry, self.enemy_name, self.enemy_stats, self.interaction)
+                        await interaction.message.edit(embed=embed, view=view)
+                    else:      
+                        embed = discord.Embed (
+                            title=f"The **{self.enemy_name}** has attacked you!"
+                        )
+                        embed.set_thumbnail(url=self.enemy_stats["image"])
+                        if critical:
+                            embed.add_field(name="", value=f"**Critical hit!** You suffer {baseDamage} damage!", inline=False)
+                        else:
+                            embed.add_field(name="", value=f"You suffer {baseDamage} damage!", inline=False)
+                        embed.add_field(name="", value=f"You have fallen! The gym quickly calls an ambulance!", inline=False)
+
+                        player_inventory = self.user_entry.setdefault("inventory", {})
+                        gold_loot_info = self.enemy_stats["loot"].get("gold", [])
+                        base_gold = gold_loot_info[2]
+
+                        gold_loss = random.randint(base_gold // 2, base_gold * 2)
+
+                        current_gold = player_inventory.get("gold", ["g", 0])[1]
+                        gold_after_loss = max(0, current_gold - gold_loss)  
+
+                        player_inventory["gold"] = ["g", gold_after_loss]
+
+                        embed.add_field(name="", value=f"You had to pay {gold_loss} gold in healthcare.", inline=False)
+
+                        user_data = load_user_data()
+                        user_id = str(interaction.user.id) 
+                        user_data[user_id].setdefault("inventory", {}).update(self.user_entry.get("inventory", {}))
+                        save_user_data(user_data)
+
+                        await interaction.message.edit(embed=embed, view=None)
+                else:
+                    health_bar = generate_health_bar(self.user_entry["hp"])
+
+                    embed = discord.Embed(
+                        title=f"{interaction.user.display_name} vs **{self.enemy_name}**",
+                        description=f"",
+                        color=discord.Color.red()
                     )
                     embed.set_thumbnail(url=self.enemy_stats["image"])
-                    if critical:
-                        embed.add_field(name="", value=f"**Critical hit!** You suffer {baseDamage} damage!", inline=False)
-                    else:
-                        embed.add_field(name="", value=f"You suffer {baseDamage} damage!", inline=False)
-                    embed.add_field(name="", value=f"The **{self.enemy_name}** has knocked you down, but your muscles won't give up! You recover 1 HP!", inline=False)
-                    view = ReviveView(self.user_entry, self.enemy_name, self.enemy_stats, self.interaction)
+                    embed.add_field(name=f"The **{self.enemy_name}** has attacked you!", value=f"You suffer {baseDamage} damage!", inline=False)
+                    embed.add_field(name=f"Your stats:", value=f"**HP**: {self.user_entry['hp']} {health_bar}", inline=False)
+                    embed.add_field(
+                    name="",
+                    value=(
+                        f"**Strength**: {self.user_entry['strength']}  |  **Agility**: {self.user_entry['agility']}\n"
+                        f"**Endurance**: {self.user_entry['endurance']}  |  **Flexibility**: {self.user_entry['flexibility']}"
+                    ),
+                    inline=False
+                    )
+
+                    view = PlayerView(self.user_entry, self.enemy_name, self.enemy_stats, self.statuses, self.interaction)
                     await interaction.message.edit(embed=embed, view=view)
-                else:      
-                    embed = discord.Embed (
-                        title=f"The **{self.enemy_name}** has attacked you!"
+            else:
+                skill_name, skill_data = None, None
+                if "skills" in self.enemy_stats:
+                    total_chance = sum(int(data[0]) for data in self.enemy_stats["skills"].values())
+                    random_roll = random.randint(1, total_chance)
+                    cumulative_chance = 0
+                    for name, data in self.enemy_stats["skills"].items():
+                        cumulative_chance += int(data[0])
+                        if random_roll <= cumulative_chance:
+                            skill_name, skill_data = name, data
+                            break
+                
+                if skill_name and skill_data:
+                    skill_description = skill_data[1]
+                    skill_type = skill_data[2]
+                    skill_effect = skill_data[3]
+
+                    if skill_type == "damage":
+                        skill_effect = int(skill_data[3])
+                        baseDamage = self.enemy_stats["attack"] + skill_effect
+                        self.user_entry["hp"] = max(0, self.user_entry["hp"] - baseDamage)
+                    elif skill_type == "ailment":
+                        self.statuses["hasStatus"] = True
+                        
+
+                if self.user_entry["hp"] <= 0:
+                    reviveChance = max(20, self.user_entry["endurance"] / 10)
+
+                    randomRoll = random.randint(0, 100)
+
+                    if randomRoll <= reviveChance:
+                        self.user_entry["hp"] += 1
+
+                        embed = discord.Embed (
+                            title=f"The **{self.enemy_name}** has used **{skill_name}**!"
+                        )
+                        embed.add_field(name="", value=skill_description, inline=False)
+                        embed.set_thumbnail(url=self.enemy_stats["image"])
+                        if skill_type == "damage":
+                            embed.add_field(name="", value=f"You suffer {baseDamage} damage!", inline=False)
+                        elif skill_type == "ailment":
+                            embed.add_field(name="", value=f"You are afflicted by **{skill_name}**!", inline=False)
+                        embed.add_field(name="", value=f"The **{self.enemy_name}** has knocked you down, but your muscles won't give up! You recover 1 HP!", inline=False)
+                        view = ReviveView(self.user_entry, self.enemy_name, self.enemy_stats, self.interaction)
+                        await interaction.message.edit(embed=embed, view=view)
+                    else:      
+                        embed = discord.Embed (
+                            title=f"The **{self.enemy_name}** has used **{skill_name}**!"
+                        )
+
+                        embed.add_field(name="", value=skill_description, inline=False)
+                        embed.set_thumbnail(url=self.enemy_stats["image"])
+                        if skill_type == "damage":
+                            embed.add_field(name="", value=f"You suffer {baseDamage} damage!", inline=False)
+                        elif skill_type == "ailment":
+                            embed.add_field(name="", value=f"You are afflicted by **{skill_name}**!", inline=False)
+                        embed.add_field(name="", value=f"You have fallen! The gym quickly calls an ambulance!", inline=False)
+
+                        player_inventory = self.user_entry.setdefault("inventory", {})
+                        gold_loot_info = self.enemy_stats["loot"].get("gold", [])
+                        base_gold = gold_loot_info[2]
+
+                        gold_loss = random.randint(base_gold // 2, base_gold * 2)
+
+                        current_gold = player_inventory.get("gold", ["g", 0])[1]
+                        gold_after_loss = max(0, current_gold - gold_loss)  
+
+                        player_inventory["gold"] = ["g", gold_after_loss]
+
+                        embed.add_field(name="", value=f"You had to pay {gold_loss} gold in healthcare.", inline=False)
+
+                        user_data = load_user_data()
+                        user_id = str(interaction.user.id) 
+                        user_data[user_id].setdefault("inventory", {}).update(self.user_entry.get("inventory", {}))
+                        save_user_data(user_data)
+
+                        await interaction.message.edit(embed=embed, view=None)
+                else:
+                    health_bar = generate_health_bar(self.user_entry["hp"])
+
+                    embed = discord.Embed(
+                        title=f"{interaction.user.display_name} vs **{self.enemy_name}**",
+                        description=f"",
+                        color=discord.Color.red()
                     )
                     embed.set_thumbnail(url=self.enemy_stats["image"])
-                    if critical:
-                        embed.add_field(name="", value=f"**Critical hit!** You suffer {baseDamage} damage!", inline=False)
-                    else:
-                        embed.add_field(name="", value=f"You suffer {baseDamage} damage!", inline=False)
-                    embed.add_field(name="", value=f"You have fallen! The gym quickly calls an ambulance!", inline=False)
+                    if skill_type == "damage":
+                        embed.add_field(name=f"{skill_description}", value=f"You suffer {baseDamage} damage!", inline=False)
+                    elif skill_type == "ailment":
+                        embed.add_field(name=f"{skill_description}", value=f"You are afflicted by **{skill_effect}**!", inline=False)
+                    embed.add_field(name=f"Your stats:", value=f"**HP**: {self.user_entry['hp']} {health_bar}", inline=False)
+                    embed.add_field(
+                    name="",
+                    value=(
+                        f"**Strength**: {self.user_entry['strength']}  |  **Agility**: {self.user_entry['agility']}\n"
+                        f"**Endurance**: {self.user_entry['endurance']}  |  **Flexibility**: {self.user_entry['flexibility']}"
+                    ),
+                    inline=False
+                    )
 
-                    player_inventory = self.user_entry.setdefault("inventory", {})
-                    gold_loot_info = self.enemy_stats["loot"].get("gold", [])
-                    base_gold = gold_loot_info[2]
-
-                    gold_loss = random.randint(base_gold // 2, base_gold * 2)
-
-                    current_gold = player_inventory.get("gold", ["g", 0])[1]
-                    gold_after_loss = max(0, current_gold - gold_loss)  
-
-                    player_inventory["gold"] = ["g", gold_after_loss]
-
-                    embed.add_field(name="", value=f"You had to pay {gold_loss} gold in healthcare.", inline=False)
-
-                    user_data = load_user_data()
-                    user_id = str(interaction.user.id) 
-                    user_data[user_id].setdefault("inventory", {}).update(self.user_entry.get("inventory", {}))
-                    save_user_data(user_data)
-
-                    await interaction.message.edit(embed=embed, view=None)
-
-            else:
-                health_bar = generate_health_bar(self.user_entry["hp"])
-
-                embed = discord.Embed(
-                    title=f"{interaction.user.display_name} vs **{self.enemy_name}**",
-                    description=f"",
-                    color=discord.Color.red()
-                )
-                embed.set_thumbnail(url=self.enemy_stats["image"])
-                embed.add_field(name=f"The **{self.enemy_name}** has attacked you!", value=f"You suffer {baseDamage} damage!", inline=False)
-                embed.add_field(name=f"Your stats:", value=f"**HP**: {self.user_entry['hp']} {health_bar}", inline=False)
-                embed.add_field(
-                name="",
-                value=(
-                    f"**Strength**: {self.user_entry['strength']}  |  **Agility**: {self.user_entry['agility']}\n"
-                    f"**Endurance**: {self.user_entry['endurance']}  |  **Flexibility**: {self.user_entry['flexibility']}"
-                ),
-                inline=False
-                )
-
-                view = PlayerView(self.user_entry, self.enemy_name, self.enemy_stats, self.interaction)
-                await interaction.message.edit(embed=embed, view=view)
-
+                    view = PlayerView(self.user_entry, self.enemy_name, self.enemy_stats, self.statuses, self.interaction)
+                    await interaction.message.edit(embed=embed, view=view)
 
         if not interaction.response.is_done():
             await interaction.response.defer()
 
 
 class PlayerView(View):
-    def __init__(self, user_entry, enemy_name, enemy_stats, interaction):
+    def __init__(self, user_entry, enemy_name, enemy_stats, statuses, interaction):
         super().__init__()
         self.user_entry = user_entry
         self.enemy_name = enemy_name
         self.enemy_stats = enemy_stats
+        self.statuses = statuses
         self.interaction = interaction
+        
 
     @discord.ui.button(label="Punch", style=discord.ButtonStyle.danger)
     async def attack_button(self, interaction: discord.Interaction, button: Button):
@@ -197,7 +300,7 @@ class PlayerView(View):
             )
             embed.set_thumbnail(url=self.enemy_stats["image"])
             embed.add_field(name="", value=f"It's the {self.enemy_name}'s turn!")
-            view = EnemyView(self.user_entry, self.enemy_name, self.enemy_stats, self.interaction)
+            view = EnemyView(self.user_entry, self.enemy_name, self.enemy_stats, self.statuses, self.interaction)
             await interaction.message.edit(embed=embed, view=view)
         else:
             baseDamage = self.user_entry['strength'] - ((self.enemy_stats["defense"] * random.randint(1, 3)) / 2)
@@ -257,7 +360,7 @@ class PlayerView(View):
                 else:
                     embed.add_field(name="", value=f"You deal {baseDamage} damage!", inline=False)
                 embed.add_field(name="", value=f"It's the {self.enemy_name}'s turn!", inline=False)
-                view = EnemyView(self.user_entry, self.enemy_name, self.enemy_stats, self.interaction)
+                view = EnemyView(self.user_entry, self.enemy_name, self.enemy_stats, self.statuses, self.interaction)
                 await interaction.message.edit(embed=embed, view=view)
 
         if not interaction.response.is_done():
@@ -293,7 +396,7 @@ class PlayerView(View):
             )
             embed.set_thumbnail(url=self.enemy_stats["image"])
             embed.add_field(name="", value=f"It's the {self.enemy_name}'s turn!")
-            view = EnemyView(self.user_entry, self.enemy_name, self.enemy_stats, self.interaction)
+            view = EnemyView(self.user_entry, self.enemy_name, self.enemy_stats, self.statuses, self.interaction)
             await interaction.message.edit(embed=embed, view=view)
         if not interaction.response.is_done():
             await interaction.response.defer()
@@ -314,6 +417,17 @@ async def dungeon(interaction, level: int):
             ephemeral=True
         )
         return
+
+    statuses = {
+        "hasStatus": False,
+        "hasExhaustion": False,
+        "hasMuscleCramps": False,
+        "hasStiffness": False,
+        "hasBurning": False,
+        "hasFatigue": False,
+        "hasDisorientation": False,
+        "hasDisease": False
+    }
 
     level_enemies = enemies_data[str(level)]
     enemy_name, enemy_stats = random.choice(list(level_enemies.items()))  
@@ -338,7 +452,7 @@ async def dungeon(interaction, level: int):
     inline=False
     )
 
-    view = PlayerView(user_entry, enemy_name, enemy_stats, interaction)
+    view = PlayerView(user_entry, enemy_name, enemy_stats, statuses, interaction)
     await interaction.response.send_message(embed=embed, view=view)
 
 def setup(command_tree):
